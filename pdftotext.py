b12560d46cae503ec9ae069cbfa5f08032986ef7
tessajo <teresa-johnsen@t-online.de>
2021-05-21 10:14:35 +0200

    Extraktion des Textes

--- pdftotext.py
+++ pdftotext.py
@@ -2,12 +2,21 @@ from decorators import timer
 import PyPDF2 as pdf
 
 from PyPDF2 import PdfFileReader as reader
+from PyPDF2.pdf import PageObject as po
 
 def importFile(fileName):
     r = reader(fileName)
     fileInfo = r.getDocumentInfo()
     pageLayout = r.getPageLayout()
     pageNum = r.getNumPages()
-    print(fileInfo,pageLayout,pageNum,sep='\n')
+    outlines = r.getOutlines() 
+    print(fileInfo,pageLayout,pageNum,outlines,sep='\n')
 
-importFile('C:/Users/teres/OneDrive/Dokumente/Studium/Master/vl/CoronaPodcasts/MDR_KCKompass/kck172.pdf')
+def extrText(fileName):
+    o = po(fileName)
+    r = reader(fileName)
+    text = o.extractText()
+    print(text,sep='\n')
+
+# importFile('C:/Users/teres/OneDrive/Dokumente/Studium/Master/vl/CoronaPodcasts/NDR_CVUpdate/SkriptFolge001.pdf')
+extrText('C:/Users/teres/OneDrive/Dokumente/Studium/Master/vl/CoronaPodcasts/NDR_CVUpdate/SkriptFolge001.pdf')
