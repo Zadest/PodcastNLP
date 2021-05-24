88eb4e5560e1162e707ff0388af486c9c8b087e4
tessajo <teresa-johnsen@t-online.de>
2021-05-24 21:33:23 +0200

    Anpassungen MDR-Skript

--- pdftotext_mdr.py
+++ pdftotext_mdr.py
@@ -1,5 +1,6 @@
 from decorators import timer
 import PyPDF2 as pdf
+import re as re
 
 from PyPDF2 import PdfFileReader as reader
 
@@ -10,4 +11,35 @@ def importFile(fileName):
     pageNum = r.getNumPages()
     print(fileInfo,pageLayout,pageNum,sep='\n')
 
-importFile('/Users/nikavolk-spiller/Desktop/kck172.pdf')
+def extrText(fileName):
+    r = reader(fileName)
+    numPages =  r.getNumPages()
+    global Text
+    Text = ''
+    for i in range(numPages):
+        page = r.getPage(i)
+        Text = Text + page.extractText()
+    # print(Text)
+
+def performRegEx(Text):
+    # Bindestrich entfernen
+    expColon = re.compile("-\n")
+    colonText = expColon.sub('',Text)
+    # Zeilenumsprung entfernen
+    expLB = re.compile('\n')
+    lbText = expLB.sub('',colonText)
+    # Bindestrich einfügen. Fehler TODO
+    # expLB = re.compile('[a-z][A-Z]')
+    # lbText = expLB.sub('[a-z]-[A-Z]',colonText)
+    # Unterteilung nach Redner
+    expCS = re.compile("\sCamillo Schumann\s")
+    expAK = re.compile("\sAlexander Kekulé\s|\sProf. Dr. med. Dr. rer. nat. Alexander S. Kekulé\s")
+    CS = expCS.sub("\nCamillo Schumann\n",lbText)
+    AK = expAK.sub("\nAlexander Kekulé\n",CS)
+    SpeakerTransformed = AK
+    print(SpeakerTransformed)
+
+
+# importFile('C:/Users/teres/OneDrive/Dokumente/Studium/Master/vl/CoronaPodcasts/MDR_KCKompass/kck172.pdf')
+extrText('C:/Users/teres/OneDrive/Dokumente/Studium/Master/vl/CoronaPodcasts/MDR_KCKompass/kck172.pdf')
+performRegEx(Text)
