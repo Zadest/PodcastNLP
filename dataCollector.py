5d2d1e6a47d597c617b757ffb8067387e10fd461
Moritz Eßer <info@horatiuslampenschirm.de>
2021-05-26 10:24:14 +0200

    Update dataCollector.py

--- dataCollector.py
+++ dataCollector.py
@@ -2,6 +2,8 @@
 # -*- coding : utf8 -*-
 
 import os
+import sys
+from threading import currentThread
 import requests
 from xml.etree.ElementTree import ElementTree, fromstring
 from decorators import timer
@@ -53,6 +55,17 @@ regex = <li class\=\"cssDownload hasNoRessort \"\n>\n<a href=\"(.*)\"
 
 """
 
+def _loading(current:int, total:int, status:str=""):
+    bar_length = 80
+    filled_length = int(round(bar_length* current/ float(total)))
+
+    percents = round((100 * current + 1) / float(total), 1)
+
+    bar = "=" * filled_length + ">" + " " * (bar_length -filled_length)
+
+    sys.stdout.write("\r[%s] %s%s ... %s\r" % (bar, percents, "%", status))
+    sys.stdout.flush()
+
 def _load_and_decode_mdr_XML(mdr_xml_url: str) -> str:
     data = ""
     try:
@@ -99,14 +112,13 @@ def _int_to_written_number(index: int):
 
     return None, None, None, None
 
-def _find_links(data:str) -> list[str]:
+def _find_links(data:str,regex_statement:str,rel_postition_to_regex:int=0) -> list[str]:
     data_list = data.split('\n')
     links = []
     for i, line in enumerate(data_list):
         if re.search(REGEX,line):
-            link = data_list[i+2].split("\"")[1]
+            link = data_list[i+rel_postition_to_regex].split("\"")[1]
             link = "https://www.mdr.de"+link
-            #print(link)
             links.append(link)
     return links
 
@@ -129,21 +141,30 @@ def check_folder_structure():
     Path(os.path.join(RAW,'ndr')).mkdir(parents=True,exist_ok=True)
 
 def create_download_list_mdr() -> list[str]:
+    print(">> loading XML from mdr")
     data = _load_and_decode_mdr_XML(MDR_XML)
+    print(">> extract data from mdr-xml")
     links = parse_mdr_xml(data)
+    total_links = len(links)
     subsite_links = []
     file_links = []
-    for link in links:
+    for i,link in enumerate(links):
+        _loading(i,total_links,status="getting subsite links")
         result = requests.get(link)
-        temp_links = _find_links(result.content.decode("utf8"))
+        temp_links = _find_links(result.content.decode("utf8"),regex_statement=r"^<li class\=\"css.*$",rel_postition_to_regex=2)
         if temp_links == []:
             continue
         else:
             subsite_links.append(temp_links)
         sleep(0.1)
+    print("")
+    
     subsite_links = _flatten_list(subsite_links)
 
-    for link in subsite_links:
+    total_links = len(subsite_links)
+
+    for i,link in enumerate(subsite_links):
+        _loading(i,total_links,status="getting download links")
         result = requests.get(link)
         data = result.content.decode("utf8").split("\n")
         for i, line in enumerate(data):
@@ -151,10 +172,12 @@ def create_download_list_mdr() -> list[str]:
                 if i >= 1:
                     temp_link = "https://www.mdr.de"+data[i-1].split("\"")[1]
                     file_links.append(temp_link)
+    
+    print("")
         
     return file_links
 
-def create_dowload_list_mdr(n:int=184):
+def create_download_list_mdr_OLD(n:int=184):
     link_list_to_pdfs = [""]
     for i in range(64,n+1):
         _,_,_,i_to_str = _int_to_written_number(i)
@@ -170,42 +193,53 @@ def create_dowload_list_mdr(n:int=184):
 
 def create_download_list_ndr(n:int=89):
     link_list = []
+    print(">> creating link list")
     for i in range(100,(89*2)+1,2):
+        _loading(i//2,89,status="adding links to ndr list")
         link_list.append("https://www.ndr.de/nachrichten/info/coronaskript"+str(i)+".pdf")
-    pprint(link_list)
+    #pprint(link_list)
     return link_list
 
 
-def request_every_link(link_list,folder_name:str):
+def request_every_link(link_list,folder_name:str,keep_name:bool=True):
     for i,element in enumerate(link_list):
+        _loading(i,len(link_list),status="downloading "+folder_name)
         try:
             element = str(element)
         except TypeError:
             print(f"Element : {element} is not type castable to str")
             break
         name = element.split("/")[-1]
-        print(f"> downloading {name}")
+        #print(f"> downloading {name}")
         try:
             result = requests.get(element)
         except ConnectionError:
             print("connection error")
         sleep(.3)
-        with open(os.path.join(os.path.join(RAW,folder_name),element.split("/")[-1]),"wb") as f:
-            f.write(result.content)
+        if keep_name:
+            with open(os.path.join(os.path.join(RAW,folder_name),element.split("/")[-1]),"wb") as f:
+                f.write(result.content)
+        else:
+            with open(os.path.join(os.path.join(RAW,folder_name),str(i)+"."+element.split("/")[-1].split(".")[-1]),"wb") as f:
+                f.write(result.content)
     return True
-
-#def check_byte_size():
-
 
 @timer
 def main():
+    print(">  checking for correct folder structure")
     check_folder_structure()
-
+    print(">  done!")
+    print(">  MDR")
     mdr_download_list = create_download_list_mdr()
-    request_every_link(mdr_download_list[1:],"mdr")
-
+    print(">> download files from MDR")
+    request_every_link(mdr_download_list,"mdr",keep_name=False)
+    print(">> done!")
+    print(">  NDR")
     ndr_download_list = create_download_list_ndr()
-    request_every_link(ndr_download_list,"ndr")
+    print(">> download files from NDR")
+    request_every_link(ndr_download_list,"ndr",keep_name=False)
+    print(">> done!")
+    print(">  all done!")
 
 if __name__ == "__main__":
     main()
